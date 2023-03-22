5bb41dec41926f1f238806a91c9c1fe7f793cabf
Manuel Nunes <manuel.nunes@vcontractor.co.za>
2023-03-22 08:42:37 +0200

    Added initial Files

---
+++ index.js
@@ -0,0 +1,41 @@
+// Main lighthouse runner / fn
+import lighthouse from 'lighthouse';
+
+// Required for launching chrome instance
+import chromeLauncher from 'chrome-launcher';
+
+// So we can save output
+import { writeFile } from 'fs/promises';
+
+const URLS = ['https://mfdot.com/','https://mfdot.com/food-app/index.html'];
+
+async function doAnal(URL)
+{
+  // Launch instance of Chrome
+  const chrome = await chromeLauncher.launch();
+
+  // Gather results and report from Lighthouse
+  const results = await lighthouse(URL, {
+    port: chrome.port,
+    output: 'json'
+  }, {
+    extends: 'lighthouse:default',
+    settings: {
+      onlyCategories: ['performance']
+    }
+  });
+
+  // Save report to file
+  await writeFile('./lighthouse-report.json', results.report);
+
+  // Kill Chrome
+  await chrome.kill();
+}
+
+async function Callme(){
+  for (let i = 0; i < URLS.length;i++){
+    await doAnal(URLS[i])
+  }
+}
+
+Callme();
